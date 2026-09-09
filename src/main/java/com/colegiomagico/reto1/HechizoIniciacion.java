e0ed22b3bedd6dd7eef36732c4fd38a3d68d84c8
Johan mateo Vasquez osorio <vasquezosoriojohanmateo@gmail.com>
2026-09-09 18:35:44 +0000

    feat: reto 1 = se agrego el nombre de usuario y del profesor

--- src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
+++ src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
@@ -6,7 +6,7 @@ package com.colegiomagico.reto1;
 public class HechizoIniciacion {
 
     public static String invocar() {
-        return ""; // TODO: Cambia esto
+        return "mi nombre es: Mateo y el nombre de mi profesor es: davo"; // TODO: Cambia esto
     }
 
     public static void main(String[] args) {
