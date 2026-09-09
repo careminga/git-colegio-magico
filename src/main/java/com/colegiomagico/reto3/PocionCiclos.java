d186d23a49a7b70ba32429c2a90a044ce372d64c
Johan mateo Vasquez osorio <vasquezosoriojohanmateo@gmail.com>
2026-09-09 18:42:19 +0000

    feat: reto 3, se añadio el ciclo for para poder saber cuantas pociones se prepararon

--- src/main/java/com/colegiomagico/reto3/PocionCiclos.java
+++ src/main/java/com/colegiomagico/reto3/PocionCiclos.java
@@ -6,7 +6,12 @@ package com.colegiomagico.reto3;
 public class PocionCiclos {
 
     public static String preparar(int cantidad) {
-        return ""; // TODO: Cambia esto
+        String pocion = "";
+        for (int i = 0; i < cantidad; i++) {
+            pocion += "pocion ";
+        }
+        return pocion;
+
     }
 
     public static void main(String[] args) {
